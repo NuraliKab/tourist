ffefab3b937fdf954581ed03a32d5e93633a0554
Nurali <nuralikabiyev@gmail.com>
2026-02-27 00:30:27 +0100

    typo

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
 
 # Add WASM target
-rustup target add wa32-unknown-unknown
+rustup target add wasm32-unknown-unknown
 
 # Download and install trunk
 wget -qO- https://github.com/trunk-rs/trunk/releases/download/v0.18.6/trunk-x86_64-unknown-linux-gnu.tar.gz | tar -xzf-
